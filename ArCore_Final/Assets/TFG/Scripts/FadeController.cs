22443f040f6340b08ac4493cc98c1c1e5df088d7
Javier Cordero <ruiderillo@hotmail.es>
2020-05-20 20:26:15 +0200

    Creado proyecto para mostras estadisticas de depuracion

---
+++ ArCore_Final/Assets/TFG/Scripts/FadeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeController : MonoBehaviour
+{
+	public DepthCamera camera;
+
+	private Scrollbar scrollbar;
+
+	private void Start()
+	{
+		scrollbar = GetComponent<Scrollbar>();
+	}
+
+	public void scrollMoved()
+	{
+		camera.depthLevel = scrollbar.value * 3;
+	}
+
+}
---
+++ ArCore_Final/Assets/TFG/Scripts/MoveCube.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCube : MonoBehaviour
+{
+	Vector3 v = new Vector3(0, 0, 0.05f);
+
+    public void cubeForward()
+	{
+		transform.localPosition += v;
+	}
+
+	public void cubeBackward()
+	{
+		transform.localPosition -= v;
+	}
+
+}
